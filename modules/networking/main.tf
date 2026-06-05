febbf82dbbb7413710474fa650f3e5e9a6ecdfd8
Gautam Kumar <gautamkumarcareers@gmail.com>
2026-06-05 11:24:09 +0530

    fix: correct VPC tags syntax

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -2,9 +2,10 @@ resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
-  tags = { 
-  Name = "${var.project}-vpc-${var.env}"
-  Project = "hospital-appointments"
+  tags = {
+    Name    = "${var.project}-vpc-${var.env}"
+    Project = "hospital-appointments"
+  }
 }
 
 resource "aws_subnet" "public" {
