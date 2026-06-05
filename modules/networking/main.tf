c89422cf73ba24ad11a8beafa6f69e7d3a39905e
Gautam Kumar <gautamkumarcareers@gmail.com>
2026-06-05 11:21:02 +0530

    infra: add project tag to VPC

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -2,7 +2,9 @@ resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
-  tags = { Name = "${var.project}-vpc-${var.env}" }
+  tags = { 
+  Name = "${var.project}-vpc-${var.env}"
+  Project = "hospital-appointments"
 }
 
 resource "aws_subnet" "public" {
